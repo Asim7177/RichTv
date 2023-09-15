d6476bb07104660ebf7c711b8cfd7d37dc309517
Asim7177 <119034806+Asim7177@users.noreply.github.com>
2023-09-15 16:46:00 +0500

    old code removed

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,42 +1,4 @@
 import React from 'react';
-import cardImage from './img/card-img.jpg';
 import './style.css';
 
-const index = () => {
-  let  card_data = [
-        {
-            img: cardImage,
-            title : 'Title card 1',
-            desc : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quam reprehenderit quod expedita dolor ratione error reiciendis voluptas cumque iste aspernatur accusantium temporibus quis, maxime dicta nemo recusandae nam veniam.'
-        },
-        {
-            img: cardImage,
-            title : 'Title card 2',
-            desc : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quam reprehenderit quod expedita dolor ratione error reiciendis voluptas cumque iste aspernatur accusantium temporibus quis, maxime dicta nemo recusandae nam veniam.'
-        },
-        {
-            img: cardImage,
-            title : 'Title card 3',
-            desc : 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ducimus quam reprehenderit quod expedita dolor ratione error reiciendis voluptas cumque iste aspernatur accusantium temporibus quis, maxime dicta nemo recusandae nam veniam.'
-        },
-    ]
-    return (
-        <>
-            <div className='cards'>
-            {
-                card_data.map(card => {
-                  return(
-                    <div className='card'>
-                        <img src={card.img} alt="card" className='card-img' />
-                        <h1>{card.title}</h1>
-                        <p>{card.desc}</p>
-                    </div>
-                  );
-                })
-            }
-            </div>
-        </>
-    )
-}
 
-export default index;
